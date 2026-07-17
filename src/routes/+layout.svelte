3fc8f460fae81495d8c34a4041bb8054a526f06b
sawlvl <wilstan.sanquer@outlook.fr>
2026-07-17 14:30:51 +0200

    feat : page bento

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,84 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header>
+	<nav>
+		<div class="menu">
+			<a href="/">Accueil</a>
+			<a href="programmes">Mes programmes</a>
+			<a href="oracle">Oracle</a>
+			<a href="contact">Me contacter</a>
+		</div>
+		<div class="social">
+			<a href="https://www.linkedin.com/in/wilstan-sanquer/" aria-label="LinkedIn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M8 11v5" /><path d="M8 8v.01" /><path d="M12 16v-5" /><path d="M16 16v-3a2 2 0 1 0 -4 0" /><path d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4l0 -10" /></svg></a>
+			<a href="https://github.com/wilstan-sanquer" aria-label="Github"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg></a>
+			<a href="https://gitlab.com/wilstan-sanquer" aria-label="Gitlab"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-gitlab"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7l3 11" /></svg></a>
+			<a href="mailto:wilstan.sanquer@outlook.fr" aria-label="Outlook"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10" /><path d="M3 7l9 6l9 -6" /></svg></a>
+		</div>
+	</nav>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer>
+	<p>Made by <a href="https://github.com/wilstan-sanquer">SAW</a>. Tous droits réservés.</p>
+	<a class="legal" href="mentions-legal">Mentions légales</a>
+</footer>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-between;
+		padding-top: 1.75rem;
+		padding-bottom: 1.75rem;
+	}
+
+	.menu {
+		display: flex;
+		gap: 30px;
+		width: 50%;
+	}
+
+	.menu a {
+		text-decoration: none;
+		color: white;
+	}
+
+	.social {
+		display: flex;
+		justify-content: end;
+		width: 50%;
+		gap: 30px;
+	}
+
+	.social a {
+		color: white;
+	}
+
+	footer {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: .75rem;
+		margin-bottom: .75rem;
+	}
+
+	footer p {
+		color: #8B93A0;
+	}
+
+	footer a {
+		text-decoration: none;
+		color: #8B93A0;
+	}
+
+	footer a:hover {
+		color: whitesmoke;
+	}
+
+	.legal {
+		text-decoration: underline;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,182 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    //Importation des images pour la page de Bento
+    import profil from '$lib/assets/profil.png';
+    import logo_archprod from '$lib/assets/logo.png';
+    import logo_oracle from '$lib/assets/oracle_logo.png'
+    import html from '$lib/assets/techno/HTML.svg';
+    import css from '$lib/assets/techno/CSS.svg';
+    import javascript from '$lib/assets/techno/JavaScript.svg';
+    import java from '$lib/assets/techno/Java.svg';
+    import svelte from '$lib/assets/techno/Svelte.svg';
+    import tailwind from '$lib/assets/techno/TailwindCSS.svg';
+</script>
+
+<main>
+    <div class="wrapper">
+        <div class="card-perso">
+            <h1>Wilstan Sanquer</h1>
+            <ul>
+                <li>Développeur Junior Frontend & Java</li>
+                <li>Étudiant BTS CIEL</li>
+            </ul>
+        </div>
+        <div class="profil">
+            <img src="{profil}" alt="PP">
+        </div>
+        <div class="card-archprod">
+            <a href="https://archprod.alwaysdata.net/" target="_blank">
+                <h1>Mon <br>Studio <br>de développement</h1>
+                <img src="{logo_archprod}" alt="Arch-Prod_logo">
+            </a>
+        </div>
+        <div class="card-oracle">
+            <h1>Oracle</h1>
+            <img src="{logo_oracle}" alt="Oracle_Bot">
+        </div>
+        <div class="techno">
+            <h1>Mes techno : </h1>
+            <div class="techno-icons">
+                <img src="{html}" alt="HTML">
+                <img src="{css}" alt="CSS">
+                <img src="{javascript}" alt="Javascript">
+                <img src="{tailwind}" alt="Tailwind">
+                <img src="{svelte}" alt="Svelte">
+                <img src="{java}" alt="Java">
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+    :global(body) {
+        background-color: #1a1a1a;
+        margin: 0 15% 10px;
+        font-family: "Red Hat Display", sans-serif; 
+    }
+
+    .wrapper {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 10px;
+        grid-auto-rows: minmax(100px, auto);
+    }
+
+    .card-perso {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: left;
+        padding: 20px;
+        border-radius: 20px;
+        background-color: #2a2a2a;
+        grid-row: 1 / 3;
+        grid-column: 1 / 3;
+    }
+
+    .card-perso h1 {
+        margin-left: 0;
+        margin-bottom: 10px;
+        padding: 0;
+        font-size: 45px;
+        color: white;
+    }
+
+    .card-perso ul {
+        list-style-position: inside;
+        margin-left: 0;
+        padding: 0;
+    }
+
+    .card-perso li {
+        font-size: 20px;
+        color: white;
+    }
+
+    .profil {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 20px;
+        background-color: #2a2a2a;
+        grid-column: 3 / 5;
+        grid-row: 1 / 3;
+    }
+
+    .profil img {
+        height: 240px;
+        width: auto;
+    }
+
+    .card-oracle {
+        display: flex;
+        flex-direction: column;
+        border-radius: 20px;
+        background: #2A2A2A;
+        background: linear-gradient(216deg, rgba(0, 0, 0, 1) 0%, rgba(169, 169, 241, 1) 100%);
+        grid-column: 1;
+        grid-row: 3 / 5;
+        align-items: center;
+    }
+    
+    .card-oracle h1 {
+        color: white;
+        text-align: center;
+    }
+
+    .card-oracle img {
+        height: 100px;
+        width: auto;
+    }
+
+    .card-archprod {
+        align-items: center;
+        padding: 20px;
+        border-radius: 20px;
+        background: 2A2A2A;
+        background: linear-gradient(165deg, rgba(0, 0, 0, 1) 0%, rgba(22, 145, 114, 1) 100%);
+        grid-column: 2 / 5;
+        grid-row: 3 / 5;
+    }
+
+    .card-archprod a {
+        text-decoration: none;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .card-archprod h1 {
+        color: white;
+    }
+
+    .card-archprod img {
+        height: 150px;
+        width: auto;
+    }
+
+    .techno {
+        display: flex;
+        align-items: center;
+        border-radius: 20px;
+        padding: 10px;
+        padding-left: 20px;
+        background-color: #2a2a2a;
+        grid-row: 5 / 6;
+        grid-column: 1 / 5;
+    }
+
+    .techno h1 {
+        margin-right: 20px;
+        color: white;
+    }
+
+    .techno-icons {
+        display: flex;
+        align-items: center;
+        gap: 30px;
+    }
+
+    .techno-icons img {
+        height: 50px;
+        width: auto;
+    }
+</style>
