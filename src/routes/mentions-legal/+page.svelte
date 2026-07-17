f12789e820049b839cc8e1e2c1dd8894d74b084d
sawlvl <wilstan.sanquer@outlook.fr>
2026-07-17 17:44:07 +0200

    feat : add content page legal mentions

---
+++ src/routes/mentions-legal/+page.svelte
@@ -0,0 +1,70 @@
+<script></script>
+
+<main>
+    <div class="cadre">
+        <h1>Mentions Légales</h1>
+        
+        <div class="contenu-mentions">
+            <p>
+                Le Site est édité par M. Wilstan Sanquer, agissant en tant que particulier et assurant également la direction de la publication. Il peut être contacté directement depuis le formulaire de contact ou via le bouton e-mail de la barre de navigation.
+            </p>
+
+            <p>
+                Le code source du site est disponible à l'adresse suivante : <a href="https://github.com/wilstan-sanquer/bento" target="_blank" rel="noopener noreferrer">wilstan-sanquer/bento</a>. Sauf mention contraire, les contenus textuels et visuels propres au projet restent la propriété exclusive de leur auteur.
+            </p>
+
+            <p>
+                Le Site est hébergé par Vercel, qui peut être contacté depuis les moyens suivants :
+                <br>• par courriel à l’adresse suivante : privacy@vercel.com ;
+                <br>• par courrier à l’adresse suivante : 340 S Lemon Ave #4133, Walnut, CA 91789, États-Unis.
+            </p>
+        </div>
+    </div>
+</main>
+
+<style>
+    .cadre {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        border-radius: 20px;
+        background-color: #2a2a2a;
+        height: 530px;
+        width: 100%;
+        padding: 40px;
+        box-sizing: border-box;
+    }
+
+    .cadre h1 {
+        font-size: 50px;
+        margin-bottom: 20px;
+    }
+
+    .cadre h1, .cadre p {
+        color: white;
+    }
+
+    .cadre p {
+        line-height: 25px;
+    }
+
+    .contenu-mentions a {
+        color: #58a6ff;
+        text-decoration: none;
+    }
+
+    .contenu-mentions a:hover {
+        text-decoration: underline;
+    }
+
+    .contenu-mentions {
+        max-width: 800px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        text-align: left;
+        width: 100%;
+    }
+</style>
