67e736e83ff905f47bdfa739040b7efc13eb683f
SAW <wilstan.sanquer@outlook.fr>
2026-08-02 18:45:36 +0200

    fix: adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-    preprocess: vitePreprocess(),
     kit: {
         adapter: adapter()
     }
