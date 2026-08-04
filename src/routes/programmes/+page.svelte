f7acd7739a809724703ec720ce803136ed9aa983
SAW <wilstan.sanquer@outlook.fr>
2026-08-04 14:28:55 +0200

    feat : add projects java-books

--- src/routes/programmes/+page.svelte
+++ src/routes/programmes/+page.svelte
@@ -4,6 +4,11 @@
 <main>
     <div class="cadre">
         <div class="programmes">
+            <h1>Java-Books</h1>
+            <p>Mon premier projet Java : un gestionnaire de bibliothèque en CLI.</p>
+            <a href="https://github.com/wilstan-sanquer/java-books" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>Voir le code source</a>
+        </div>
+        <div class="programmes">
             <h1>Rust-Calculatrice</h1>
             <p>Mon premier projet Rust : une calculatrice en CLI.</p>
             <a href="https://github.com/wilstan-sanquer/rust-calc" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>Voir le code source</a>
@@ -47,7 +52,7 @@
         align-items: center;
         border: 1px solid #1a1a1a;
         border-radius: 20px;
-        margin: 40px;
+        margin: 20px;
         padding: 20px;
         color: white;
         background-color: #1a1a1a;
